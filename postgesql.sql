b820d747a0a6e93893e3c70fae8a59d60a8e7c85
mmaria <79855523+MariaDziuina@users.noreply.github.com>
2021-10-28 15:12:21 +0300

    Update postgesql.sql

--- postgesql.sql
+++ postgesql.sql
@@ -6,3 +6,8 @@ SELECT *
   FROM information_schema.columns
  WHERE table_schema = 'amplitude'
    AND table_name   = 'users'
+
+-- извлечение недели, месяца
+, to_char(a.event_date, 'YYYY-WW') as week
+, to_char(a.event_date, 'YYYY-MM') as month
+, date_trunc('week', a.event_date::date)::date as week_start_date

f63d761bc9e1a435c1d904c561a383d1690cbec1
mmaria <79855523+MariaDziuina@users.noreply.github.com>
2022-02-09 14:37:37 +0300

    Update postgesql.sql

--- postgesql.sql
+++ postgesql.sql
@@ -1,4 +1,5 @@
-
+ -- Округление времени вниз до секунд:
+to_char(bg.event_time, 'YYYY-MM-DD HH24:MI:SS".000"') event_time_round
 
 
 -- ингформация по таблице (типы данных и проч)
@@ -11,3 +12,7 @@ SELECT *
 , to_char(a.event_date, 'YYYY-WW') as week
 , to_char(a.event_date, 'YYYY-MM') as month
 , date_trunc('week', a.event_date::date)::date as week_start_date
+
+
+-- проверка статусов запущенных запросов
+SELECT * FROM pg_stat_activity

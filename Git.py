77219bf849f93dacc41c39a469e8ef4260937372
mmaria <79855523+MariaDziuina@users.noreply.github.com>
2022-05-17 20:50:29 +0300

    Create Git.py

---
+++ Git.py
@@ -0,0 +1,6 @@
+
+
+Если есть проблемы с установкой пакета, импортом функций - возможно следует указать явно путь
+
+import sys
+sys.path.append('C:\\Users\\arx\\Documents\\Maria\\deeplay_python_common\\deeplay\\analytics')

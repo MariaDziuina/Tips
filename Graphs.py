09c937e70386b0cdd0430945e51225e1c1e777bf
mmaria <79855523+MariaDziuina@users.noreply.github.com>
2023-10-11 23:04:57 +0300

    Update Graphs.py
    ри

--- Graphs.py
+++ Graphs.py
@@ -104,41 +104,6 @@ def level_interest_plot(df, title):
     ax.legend()
 
 
-
-
-
-
-###################
-### ОБЛАКО СЛОВ ###
-
-# подготовка структуры данных для облака слов
-# отправляет слова, которые встретили не менее n раз
-def prep_gr_word_cloud(words_df, group, n):
-    common=words_df[
-        (words_df['words']>n)&
-        (words_df['group']==group)&
-        # загулшку остутсвия данных не выводим
-        (words_df['index']!='no')&(words_df['index']!='info')
-            ][['index', 'words']].set_index('index').to_dict()['words']
-    return common
-# функция строит облако слов и сохраняет её в файл
-# для работы нужно pip install wordcloud
-# https://github.com/amueller/word_cloud
-def write_cloud(dct, file):
-    from wordcloud import WordCloud
-    from matplotlib import pyplot as plt
-    wc = WordCloud(width=2600, height=2200, background_color="black", relative_scaling=1.0,
-               collocations=False, min_font_size=10).generate_from_frequencies(dict(dct))
-    plt.axis("off")
-    plt.figure(figsize=(9, 6))
-    plt.imshow(wc, interpolation="bilinear")
-    plt.xticks([])
-    plt.yticks([])
-    plt.tight_layout()
-    plt.savefig(file, format="png")
-#     plt.show()
-
-
 ######
 # plotly
 # линейный график с дов.интервалами
@@ -175,6 +140,41 @@ fig.add_trace(
 
 fig.update_layout(hovermode = 'x')
 fig.show()
+
+
+
+
+###################
+### ОБЛАКО СЛОВ ###
+
+# подготовка структуры данных для облака слов
+# отправляет слова, которые встретили не менее n раз
+def prep_gr_word_cloud(words_df, group, n):
+    common=words_df[
+        (words_df['words']>n)&
+        (words_df['group']==group)&
+        # загулшку остутсвия данных не выводим
+        (words_df['index']!='no')&(words_df['index']!='info')
+            ][['index', 'words']].set_index('index').to_dict()['words']
+    return common
+# функция строит облако слов и сохраняет её в файл
+# для работы нужно pip install wordcloud
+# https://github.com/amueller/word_cloud
+def write_cloud(dct, file):
+    from wordcloud import WordCloud
+    from matplotlib import pyplot as plt
+    wc = WordCloud(width=2600, height=2200, background_color="black", relative_scaling=1.0,
+               collocations=False, min_font_size=10).generate_from_frequencies(dict(dct))
+    plt.axis("off")
+    plt.figure(figsize=(9, 6))
+    plt.imshow(wc, interpolation="bilinear")
+    plt.xticks([])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig(file, format="png")
+#     plt.show()
+
+
 
 
 
